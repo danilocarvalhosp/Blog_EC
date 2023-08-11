58d419dd5cfe501ef6ede09ee3bbddef19fd37e2
Danilo Carvalho <danilocarvalhosp@gmail.com>
2023-08-11 20:23:22 -0300

    Create

--- Blog_EC/Data/BlogDataContext.cs
+++ Blog_EC/Data/BlogDataContext.cs
@@ -12,13 +12,15 @@ namespace Blog_EC.Data
     {
         public DbSet<Category> Categories { get; set; }
         public DbSet<Post> Posts { get; set; }
-        public DbSet<PostTag> PostTags { get; set; }
+
+        //public DbSet<PostTag> PostTags { get; set; }
         public DbSet<Role> Roles { get; set; }
         public DbSet<Tag> Tags { get; set; }
         public DbSet<User> Users { get; set; }
-        public DbSet<UserRole> UserRoles { get; set; }
+
+        //public DbSet<UserRole> UserRoles { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder options)
-            => options.UseSqlServer(@"Server=localhost,1433;Database=Blog;User ID=sa;Password=Dan@8257;Trusted_Connection=False; TrustServerCertificate=True;");
+            => options.UseSqlServer(@"Server=localhost,1433;Database=Blog_EC;User ID=sa;Password=Dan@8257;Trusted_Connection=False; TrustServerCertificate=True;");
     }
 }
--- Blog_EC/Program.cs
+++ Blog_EC/Program.cs
@@ -1,7 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using Blog_EC.Data;
+using Blog_EC.Models;
 
-using(var context = new BlogDataContext())
+using (var context = new BlogDataContext())
 {
-
+    var tag = new Tag { Name = "ASP.NET", Slug = "aspnet" };
+    context.Tags.Add(tag);
+    context.SaveChanges();
 }
