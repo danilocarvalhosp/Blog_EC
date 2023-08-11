8ccec9890829606d18efa65cdf493860a1e84abd
Danilo Carvalho <danilocarvalhosp@gmail.com>
2023-08-11 20:27:40 -0300

    Update

--- Blog_EC/Program.cs
+++ Blog_EC/Program.cs
@@ -4,7 +4,15 @@ using Blog_EC.Models;
 
 using (var context = new BlogDataContext())
 {
-    var tag = new Tag { Name = "ASP.NET", Slug = "aspnet" };
-    context.Tags.Add(tag);
+    // CREATE
+    //var tag = new Tag { Name = "ASP.NET", Slug = "aspnet" };
+    //context.Tags.Add(tag);
+    //context.SaveChanges();
+
+    // UPDATE
+    var tag = context.Tags.FirstOrDefault(x => x.Id == 1);
+    tag.Name = ".NET";
+    tag.Slug = "dotnet";
+    context.Update(tag);
     context.SaveChanges();
 }
