cb59c88c508892f40b4fc29d8a684525aa53519f
Danilo Carvalho <danilocarvalhosp@gmail.com>
2023-08-11 20:10:10 -0300

    DbSet

---
+++ Blog_EC/Data/BlogDataContext.cs
@@ -0,0 +1,15 @@
+﻿using Blog_EC.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blog_EC.Data
+{
+    public class BlogDataContext : DbContext
+    {
+        public DbSet<Category> Categories { get; set; }
+    }
+}
