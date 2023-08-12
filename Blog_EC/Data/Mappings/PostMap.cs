5e9e200e7bd6c0390602f6f70a57a5e30f7e9503
Danilo Carvalho <danilocarvalhosp@gmail.com>
2023-08-12 15:25:33 -0300

    Relacionamento Muitos para Muitos

--- Blog_EC/Data/Mappings/PostMap.cs
+++ Blog_EC/Data/Mappings/PostMap.cs
@@ -47,6 +47,21 @@ namespace Blog_EC.Data.Mappings
                 .WithMany(x => x.Posts)
                 .HasConstraintName("FK_Post_Category")
                 .OnDelete(DeleteBehavior.Cascade);
+
+            builder.HasMany(x => x.Tags)
+                .WithMany(x => x.Posts)
+                .UsingEntity<Dictionary<string, object>>(
+                    "PostTag",
+                    post => post.HasOne<Tag>()
+                        .WithMany()
+                        .HasForeignKey("PostId")
+                        .HasConstraintName("FK_PostTag_PostId")
+                        .OnDelete(DeleteBehavior.Cascade),
+                    tag => tag.HasOne<Post>()
+                        .WithMany()
+                        .HasForeignKey("TagId")
+                        .HasConstraintName("FK_PostTag_TagId")
+                        .OnDelete(DeleteBehavior.Cascade));
         }
     }
 }
