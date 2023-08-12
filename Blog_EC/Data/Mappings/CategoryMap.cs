7fd18f7dd5904b11fb487d3d681ac955195e619d
Danilo Carvalho <danilocarvalhosp@gmail.com>
2023-08-12 14:36:59 -0300

    Mapeando propriedades

--- Blog_EC/Data/Mappings/CategoryMap.cs
+++ Blog_EC/Data/Mappings/CategoryMap.cs
@@ -23,6 +23,19 @@ namespace Blog_EC.Data.Mappings
             builder.Property(x => x.Id)
                 .ValueGeneratedOnAdd()
                 .UseIdentityColumn(); // Primary Key Identity (1, 1)
+
+            // Propriedades
+            builder.Property(x => x.Name)
+                .IsRequired() // Not Null
+                .HasColumnName("Name")
+                .HasColumnType("NVARCHAR")
+                .HasMaxLength(80);
+
+            builder.Property(x => x.Slug)
+                .IsRequired() // Not Null
+                .HasColumnName("Slug")
+                .HasColumnType("VARCHAR")
+                .HasMaxLength(80);
         }
     }
 }
