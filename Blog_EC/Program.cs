856fcbc30d2924f0db28c4e813e9b7ca11942e94
Danilo Carvalho <danilocarvalhosp@gmail.com>
2023-08-12 10:11:14 -0300

    Trabalhando com Sub Conjuntos

--- Blog_EC/Program.cs
+++ Blog_EC/Program.cs
@@ -3,7 +3,35 @@ using Blog_EC.Data;
 using Blog_EC.Models;
 using Microsoft.EntityFrameworkCore;
 
-using (var context = new BlogDataContext())
-{
+using var context = new BlogDataContext();
 
-}
+var user = new User
+{
+    Name = "André Baltieri",
+    Slug = "andrebaltieri",
+    Email = "andre@balta.io",
+    Bio = "9x Microsoft MVP",
+    Image = "https://balta.io",
+    PasswordHash = "123456789"
+};
+
+var category = new Category
+{
+    Name = "Backend",
+    Slug = "backend"
+};
+
+var post = new Post
+{
+    Author = user,
+    Category = category,
+    Body = "<p>Hello world</p>",
+    Slug = "comecando-com-ef-core",
+    Summary = "Neste artigo vamos aprender EF Core",
+    Title = "Começando com EF Core",
+    CreateDate = DateTime.Now,
+    LastUpdateDate = DateTime.Now,
+};
+
+context.Posts.Add(post);
+context.SaveChanges();
