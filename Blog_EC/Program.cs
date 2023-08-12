4b7575e43df3eda0b65d07df6daeb20716225e01
Danilo Carvalho <danilocarvalhosp@gmail.com>
2023-08-12 10:20:57 -0300

    Include

--- Blog_EC/Program.cs
+++ Blog_EC/Program.cs
@@ -5,33 +5,46 @@ using Microsoft.EntityFrameworkCore;
 
 using var context = new BlogDataContext();
 
-var user = new User
-{
-    Name = "André Baltieri",
-    Slug = "andrebaltieri",
-    Email = "andre@balta.io",
-    Bio = "9x Microsoft MVP",
-    Image = "https://balta.io",
-    PasswordHash = "123456789"
-};
+#region Trabalhando com Sub Conjunto
+//var user = new User
+//{
+//    Name = "André Baltieri",
+//    Slug = "andrebaltieri",
+//    Email = "andre@balta.io",
+//    Bio = "9x Microsoft MVP",
+//    Image = "https://balta.io",
+//    PasswordHash = "123456789"
+//};
 
-var category = new Category
-{
-    Name = "Backend",
-    Slug = "backend"
-};
+//var category = new Category
+//{
+//    Name = "Backend",
+//    Slug = "backend"
+//};
 
-var post = new Post
-{
-    Author = user,
-    Category = category,
-    Body = "<p>Hello world</p>",
-    Slug = "comecando-com-ef-core",
-    Summary = "Neste artigo vamos aprender EF Core",
-    Title = "Começando com EF Core",
-    CreateDate = DateTime.Now,
-    LastUpdateDate = DateTime.Now,
-};
+//var post = new Post
+//{
+//    Author = user,
+//    Category = category,
+//    Body = "<p>Hello world</p>",
+//    Slug = "comecando-com-ef-core",
+//    Summary = "Neste artigo vamos aprender EF Core",
+//    Title = "Começando com EF Core",
+//    CreateDate = DateTime.Now,
+//    LastUpdateDate = DateTime.Now,
+//};
 
-context.Posts.Add(post);
-context.SaveChanges();
+//context.Posts.Add(post);
+//context.SaveChanges();
+#endregion
+
+var posts = context
+    .Posts
+    .Where (x => x.AuthorId == 1)
+    .Include (x => x.Author)
+    .AsNoTracking()
+    .OrderByDescending(x => x.LastUpdateDate)
+    .ToList();
+
+foreach (var post in posts)
+    Console.WriteLine($"{post.Title} escrito por {post.Author?.Name}");
