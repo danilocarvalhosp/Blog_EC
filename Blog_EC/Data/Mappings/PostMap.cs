2764bf86f06d018dd6f34a358d7a92088db225ec
Danilo Carvalho <danilocarvalhosp@gmail.com>
2023-08-12 15:14:23 -0300

    Relacionamento Um para Muitos

--- Blog_EC/Data/Mappings/PostMap.cs
+++ Blog_EC/Data/Mappings/PostMap.cs
@@ -36,6 +36,17 @@ namespace Blog_EC.Data.Mappings
             // Índices
             builder.HasIndex(x => x.Slug, "IX_Post_Slug")
                 .IsUnique();
+
+            // Relacionamentos
+            builder.HasOne(x => x.Author)
+                .WithMany(x => x.Posts)
+                .HasConstraintName("FK_Post_Author")
+                .OnDelete(DeleteBehavior.Cascade);
+
+            builder.HasOne(x => x.Category)
+                .WithMany(x => x.Posts)
+                .HasConstraintName("FK_Post_Category")
+                .OnDelete(DeleteBehavior.Cascade);
         }
     }
 }
