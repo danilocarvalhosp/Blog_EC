c69ed005b7bbfa4f6c6d9f3cbd67e2f5e86ae986
Danilo Carvalho <danilocarvalhosp@gmail.com>
2023-08-12 09:59:21 -0300

    Navigation Properties

--- Blog_EC/Models/User.cs
+++ Blog_EC/Models/User.cs
@@ -1,19 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Blog_EC.Models
 {
+    [Table("User")]
     public class User
     {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public string Email { get; set; }
-        public string PasswordHash { get; set; }
-        public string Image { get; set; }
-        public string Slug { get; set; }
-        public string Bio { get; set; }
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+            public int Id { get; set; }
+            public string Name { get; set; }
+            public string Email { get; set; }
+            public string PasswordHash { get; set; }
+            public string Image { get; set; }
+            public string Slug { get; set; }
+            public string Bio { get; set; }
     }
 }
