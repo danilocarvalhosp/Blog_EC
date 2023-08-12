a187133bd3a6f89a2006448bc1b17c1e9bb2583b
Danilo Carvalho <danilocarvalhosp@gmail.com>
2023-08-12 15:07:56 -0300

    Default Values

---
+++ Blog_EC/Data/Mappings/PostMap.cs
@@ -0,0 +1,41 @@
+﻿using Blog_EC.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blog_EC.Data.Mappings
+{
+    internal class PostMap : IEntityTypeConfiguration<Post>
+    {
+        public void Configure(EntityTypeBuilder<Post> builder)
+        {
+            // Tabela
+            builder.ToTable("Post");
+
+            // Chave Primária
+            builder.HasKey(x => x.Id);
+
+            // Identity
+            builder.Property(x => x.Id)
+                .ValueGeneratedOnAdd()
+                .UseIdentityColumn();
+
+            // Propriedades
+            builder.Property(x => x.LastUpdateDate)
+                .IsRequired()
+                .HasColumnName("LastUpdateDate")
+                .HasColumnType("SMALLDATETIME")
+                .HasDefaultValueSql("GETDATE()");
+            //  .HasDefaultValue(DateTime.Now.ToUniversalTime());
+
+
+            // Índices
+            builder.HasIndex(x => x.Slug, "IX_Post_Slug")
+                .IsUnique();
+        }
+    }
+}
