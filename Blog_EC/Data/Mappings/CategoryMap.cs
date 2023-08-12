9856b3032fbc1e0a21466c20f3568a4d482659e1
Danilo Carvalho <danilocarvalhosp@gmail.com>
2023-08-12 14:29:24 -0300

    Iniciando o mapeamento

---
+++ Blog_EC/Data/Mappings/CategoryMap.cs
@@ -0,0 +1,20 @@
+﻿using Blog_EC.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blog_EC.Data.Mappings
+{
+    public class CategoryMap : IEntityTypeConfiguration<Category>
+    {
+        public void Configure(EntityTypeBuilder<Category> builder)
+        {
+            // Tabela
+            builder.ToTable("Category");
+        }
+    }
+}
